6c20455765aff59da72378abda02a85e02d64eb6
MahiMoorthy <mahipmoorthy2024@outlook.com>
2025-07-13 14:30:57 -0500

    Update Product.java

--- src/main/java/com/example/productcatalog/model/Product.java
+++ src/main/java/com/example/productcatalog/model/Product.java
@@ -1,1 +1,18 @@
 
+package com.example.productcatalog.model;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document("products")
+public class Product {
+    @Id
+    private String id;
+    private String name;
+    private String category;
+    private double price;
+}
